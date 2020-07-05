fc7d20bf413231f9cdf93785763d44ef86d3e0d4
Suchadafon19 <Suchada.deechotchuang@mail.kmutt.ac.th>
2020-07-05 20:53:19 +0700

    Test1

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,46 +1,74 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+<div>
+     <div class="container-fluid contact">
+                  <div class="row">
+                     <div class="col-md-10 offset-md-1">
+                          <p class="back">BACK</p>
+                     </div>
+                  </div>
+               </div>
+               <div id="line" class="offset-md-1 col-md-12">
+               <hr color=#f9891b>
+               </div>
+     <div class="container-fluid contact">
+                     <div class="row">
+                        <div class="offset-md-1 col-md-10 ">
+                           <div class="row">
+                              <div class="col-md-9 col-sm-12">
+                                 <div class="mypic">
+                                    <img alt="Vue logo" src="../assets/Mask Group 13.jpg">
+                                 </div>
+                              </div>
+
+                              <div class="col-md-3 col-sm-12">
+                                 <p class="myname">
+
+                                    <br>
+                                   uigi
+                                    <br>
+                                    uiguigu
+                                 </p>
+
+                                 <p class="ui-detail">dfdge </p>
+                              
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+</div>
+
 </template>
 
+
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  export default {
+    data () {
+      return {
+        bottomNav: 'recent',
+      }
+    },
   }
-}
 </script>
+
+
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.back {
+  width: 57px;
+  height: 26px;
+  font-size: 20px;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1.3;
+  letter-spacing: 2px;
+  text-align: left;
+  margin-left: 60px;
+  color: #f9891b;
+}
+
 h3 {
   margin: 40px 0 0;
 }
